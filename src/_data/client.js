7037e4ccdc061e6d0e1d4bdeaf3a6a7e5d3a4be1
Danny Skelley <dannyskelley@gmail.com>
2025-09-08 09:16:36 -0400

    a
    a

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,6 +1,6 @@
 module.exports = {
-    name: "Pro Tubs LLC",
-    email: "bathtubs76@gmail.com",
+    name: "Fleur de Lis Salon",
+    email: "fdlgirl65@gmail.com",
     phoneForTel: "386.760.3515",
     phoneFormatted: "386.760.3515",
     address: {
@@ -10,7 +10,7 @@ module.exports = {
         state: "MI",
         zip: "48195",
         country: "US",
-        mapLink: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d376060.03206968214!2d-83.30923325!3d42.57718044999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa5c04c4ad6f2a3d5%3A0xa643d5c56ad54a24!2sA%20and%20A%20Outdoor%20LLC!5e0!3m2!1sen!2sus!4v1752513334459!5m2!1sen!2sus",
+        mapLink: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13766850.458597919!2d-88.43554367168856!3d32.60491823015112!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e6d820acdc45e5%3A0xebc325d202a93e53!2sFleur%20De%20Lis%20Salon!5e0!3m2!1sen!2sus!4v1757336632974!5m2!1sen!2sus",
     },
     socials: {
         facebook: "https://www.facebook.com/Providencemichigan/",
